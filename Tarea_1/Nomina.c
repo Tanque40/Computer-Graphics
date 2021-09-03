447ecf8af41211e6516acb0c0cf68b0286c08024
Bruno Vitte <bruno_vitte@hotmail.com>
2021-09-03 17:10:00 -0500

    Ya casi acabo#

--- Tarea_1/Nomina.c
+++ Tarea_1/Nomina.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//  ______                              _   _  _  _    _
+//  | ___ \                            | | | |(_)| |  | |
+//  | |_/ / _ __  _   _  _ __    ___   | | | | _ | |_ | |_   ___
+//  | ___ \| '__|| | | || '_ \  / _ \  | | | || || __|| __| / _ \
+//  | |_/ /| |   | |_| || | | || (_) | \ \_/ /| || |_ | |_ |  __/
+//  \____/ |_|    \__,_||_| |_| \___/   \___/ |_| \__| \__| \___|
+//
+//
+
 typedef struct direccion DIR;
 typedef struct empleado EMP;
 
@@ -25,24 +34,70 @@ struct empleado {
 
 EMP *crear_empleado();
 DIR nueva_direccion();
-int insertar_empleado(EMP *lista_empleados);
-int borrar_empleado(int id_empleado_to_delete, EMP *lista_empleados);
+int insertar_empleado(EMP **lista_empleados);
+int borrar_empleado(int id_empleado_to_delete, EMP **lista_empleados);
 int num_empleados(EMP *lista_empleados);
-char *mostrar_empleado(int id_empleado_to_show, EMP *lista_empleados);
-char *mostrar_direccion(EMP *empleado);
-char *los_empleados(EMP *lista_empleados);
+void mostrar_empleado(int id_empleado_to_show, EMP *lista_empleados);
+void mostrar_direccion(EMP *empleado);
+void los_empleados(EMP *lista_empleados);
 
 int main() {
-    EMP empleados;
-    EMP *apt_empleados;
-    empleados.siguiente_empleado = NULL;
+    EMP *apt_empleados = NULL;
 
-    apt_empleados = &empleados;
+    int ans = 1, id_empleado;
+
+    printf("//  ______                              _   _  _  _    _               //\n");
+    printf("//  | ___ \\                            | | | |(_)| |  | |              //\n");
+    printf("//  | |_/ / _ __  _   _  _ __    ___   | | | | _ | |_ | |_   ___       //\n");
+    printf("//  | ___ \\| '__|| | | || '_ \\  / _ \\  | | | || || __|| __| / _ \\      //\n");
+    printf("//  | |_/ /| |   | |_| || | | || (_) | \\ \\_/ /| || |_ | |_ |  __/      //\n");
+    printf("//  \\____/ |_|    \\__,_||_| |_| \\___/   \\___/ |_| \\__| \\__| \\___|      //\n");
+
+    while (ans) {
+        printf("\n\n\n\n¿Qué deseas hacer?\n");
+        printf("\n\t1) Agniadir nuevo empleado.\n");
+        printf("\n\t2) Borrar empleado (por clave).\n");
+        printf("\n\t3) Mostrar empleado (por clave). \n");
+        printf("\n\t4) Listar todos los empleados. \n");
+        printf("\n\t5) Numero de empleados. \n");
+        printf("\n\t0) SALIR\n");
+
+        printf("\nTu respuesta: ");
+        scanf("%d", &ans);
+
+        switch (ans) {
+            case 1:
+                insertar_empleado(apt_empleados);
+                break;
+            case 2:
+                printf("\nBorrar empleado. Ingresa la clave del empleado a borrar: ");
+                scanf("%d", id_empleado);
+                borrar_empleado(id_empleado, apt_empleados);
+                break;
+            case 3:
+                printf("\nMostrar empleado. Ingresa la clave del empleado a mostrar: ");
+                scanf("%d", id_empleado);
+                mostrar_empleado(id_empleado, apt_empleados);
+                break;
+            case 4:
+                los_empleados(apt_empleados);
+                break;
+            case 5:
+                num_empleados(apt_empleados);
+                break;
+            case 0:
+                ans = 0;
+                break;
+            default:
+                printf("Valor fuera de rango");
+                break;
+        }
+    }
 
     return 0;
 }
 
-int insertar_empleado(EMP *lista_empleados) {
+int insertar_empleado(EMP **lista_empleados) {
     EMP *current_employee, *nuevo_empleado;
 
     current_employee = lista_empleados;
@@ -80,6 +135,7 @@ EMP *crear_empleado() {
 
     apt_nuevo_empleado = (EMP *)malloc(sizeof(EMP));
     if (apt_nuevo_empleado == NULL) {
+        printf("Error in new empleado");
         return apt_nuevo_empleado;  // ! Aquí hay que lanzar una excepción
     }
 
@@ -94,13 +150,13 @@ EMP *crear_empleado() {
 
     nuevo_empleado.direccion_empleado = nueva_direccion();
 
-    printf("Ingresa el estado civil del empleado: (s) Soltero - (c) Casado - (u) Union Libre - (n) Prefiero no decir");
+    printf("Ingresa el estado civil del empleado: (s) Soltero - (c) Casado - (u) Union Libre - (n) Prefiero no decir: ");
     scanf("%s", &nuevo_empleado.estado_civil, 1);
 
     printf("Número de hijos del empleado: ");
     scanf("%d", &nuevo_empleado.numero_hijos);
 
-    printf("Ingresa el genero del empleado: (m) Masculino - (f) Femenino - (n) Prefiero no decir");
+    printf("Ingresa el genero del empleado: (m) Masculino - (f) Femenino - (n) Prefiero no decir: ");
     scanf("%s", &nuevo_empleado.gender);
 
     printf("Ingresa el salario del empleado: $");
@@ -113,7 +169,7 @@ EMP *crear_empleado() {
     return apt_nuevo_empleado;
 }
 
-int borrar_empleado(int id_empleado_to_delete, EMP *lista_empleados) {
+int borrar_empleado(int id_empleado_to_delete, EMP **lista_empleados) {
     EMP *current_employee;
 
     current_employee = lista_empleados;
@@ -161,98 +217,75 @@ int num_empleados(EMP *lista_empleados) {
     return cont;
 }
 
-char *mostrar_empleado(int id_empleado_to_show, EMP *lista_empleados) {
-    char *message;
+void mostrar_empleado(int id_empleado_to_show, EMP *lista_empleados) {
     int flag = 0;
     EMP *current_employee = lista_empleados;
 
-    while (!flag && current_employee == NULL) {
+    while (!flag && current_employee != NULL) {
         if (current_employee->id_empleado == id_empleado_to_show) {
             flag = 1;
         }
     }
 
     if (flag) {
-        message = (char *)malloc(sizeof("Empleado: " + current_employee->id_empleado +
-                                        "\nNombre: " + current_employee->nombre_empleado +
-                                        "\nApellidos: " + current_employee->apellido_empleado +
-                                        "\nDireccion" + mostrar_direccion(current) +
-                                        "\nSalario: " + current_employee->salario +
-                                        "\nGenero: " + current_employee->salario +
-                                        "\nEstado Civil: " + current_employee->estado_civil +
-                                        "\nNumero de hijos: " + current_employee->numero_hijos));
-        message = "Empleado: " + current_employee->id_empleado +
-                  "\nNombre: " + current_employee->nombre_empleado +
-                  "\nApellidos: " + current_employee->apellido_empleado +
-                  "\nDireccion" + mostrar_direccion(current) +
-                  "\nSalario: " + current_employee->salario +
-                  "\nGenero: " + current_employee->salario +
-                  "\nEstado Civil: " + current_employee->estado_civil +
-                  "\nNumero de hijos: " + current_employee->numero_hijos;
+        printf("Empelado: \t%s\n", current_employee->id_empleado);
+        printf("Nombre: \t%s\n", current_employee->nombre_empleado);
+        printf("Apellidos: \t%s\n", current_employee->apellido_empleado);
+        printf("Salario: \t%f\n", current_employee->salario);
+        printf("Genero: \t%s\n", current_employee->gender);
+        printf("Estado Civil: \t%s\n", current_employee->estado_civil);
+        printf("Numero de hijos: \t%d\n", current_employee->numero_hijos);
+        mostrar_direccion(current_employee);
     } else {
-        message = (char *)malloc(sizeof("No Se encontró el empleado"));
-        message = "No Se encontró el empleado";
+        printf("No Se encontró el empleado");
     }
-
-    return message;
 }
 
-char *los_empleados(EMP *lista_empleados) {
-    char *lista, *empleado;
-
+void los_empleados(EMP *lista_empleados) {
     EMP *current = lista_empleados;
 
     if (current->siguiente_empleado == NULL) {
-        lista = (char *)malloc(sizeof("No hay empleados"))
+        printf("No hay empleados");
     } else {
         while (current != NULL) {
-            empleado = (char *)malloc(sizeof(mostrar_empleado(current->id_empleado, current)));
-            empleado = mostrar_empleado(current->id_empleado, current);
-            lista = (char *)malloc(sizeof(lista + "\n ------- \n " + empleado));
-            lista = lista + "\n ------- \n " + empleado;
+            printf("\n__________________________________________________________________\n");
+            mostrar_empleado(current->id_empleado, current);
+            current = current->siguiente_empleado;
         }
     }
-
-    return lista;
 }
 
 DIR nueva_direccion() {
     DIR direccion;
 
-    printf("Calle: ");
+    printf("\nIngresa la direccion: \n\n");
+    printf("\tCalle: ");
     scanf("%s", direccion.calle);
 
-    printf("Numero: ");
+    printf("\tNumero: ");
     scanf("%d", &direccion.numero);
 
-    printf("Ciudad: ");
+    printf("\tCiudad: ");
     scanf("%s", direccion.ciudad);
 
-    printf("Codigo Postal: ");
+    printf("\tCodigo Postal: ");
     scanf("%d", &direccion.codigo_postal);
 
-    printf("Pais: ");
+    printf("\tPais: ");
     scanf("%s", direccion.pais);
+
+    printf("\n");
 
     return direccion;
 }
 
-char *mostrar_direccion(EMP *empleado) {
-    char *direccion;
-    DIR *direc = empleado->direccion_empleado;
+void mostrar_direccion(EMP *empleado) {
+    DIR direc = empleado->direccion_empleado;
 
-    direccion = (char *)malloc(sizeof(
-        "\t\nCalle: " + direc->calle +
-        "\t\nNumero: " + direc->numero +
-        "\t\nCodigo postal: " + direc->codigo_postal +
-        "\t\nCiudad: " + direc->ciudad +
-        " \t\nPaís: " + direc->pais));
-
-    direccion = "\t\nCalle: " + direc->calle +
-                "\t\nNumero: " + direc->numero +
-                "\t\nCodigo postal: " + direc->codigo_postal +
-                "\t\nCiudad: " + direc->ciudad +
-                " \t\nPaís: " + direc->pais;
-
-    return direccion
+    printf("Dirección:\n");
+    printf("\tCalle: \t%s", direc.calle);
+    printf("\tNumero: \t%d", direc.numero);
+    printf("\tCodigo Postal: \t%d", direc.codigo_postal);
+    printf("\tCiudad: \t%s", direc.ciudad);
+    printf("\tPaís: \t%s", direc.pais);
 }

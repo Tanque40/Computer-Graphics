06b4d56de02311f88a33c910c6e0912d25f09816
Bruno Vitte <bruno.vitte@itam.mx>
2021-10-22 19:04:19 -0500

    trabajando en el proyecto

---
+++ Proyecto_1/Proyecto_1/src/Application.cpp
@@ -0,0 +1,166 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+#include "Renderer.h"
+
+#include "VertexBufferLayout.h"
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
+#include "VertexArray.h"
+#include "Shader.h"
+
+#include "Penrose.h"
+
+
+void framebuffer_size_callback( GLFWwindow *window, int width, int height );
+void processInput( GLFWwindow *window );
+
+int main( void ){
+    GLFWwindow *window;
+
+    /* Initialize the library */
+    if( !glfwInit() )
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 4 );
+    glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+    
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow( 920, 920, "Proyecto_1", NULL, NULL );
+    if( !window ){
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent( window );
+    glfwSetFramebufferSizeCallback( window, framebuffer_size_callback );
+
+    glfwSwapInterval( 1 );
+
+    // Se debe poner después del make context eso marca la documentación
+    if( glewInit() != GLEW_OK )
+        std::cout << "Error!" << std::endl;
+
+    std::cout << glGetString( GL_VERSION ) << std::endl;
+
+    {
+
+        Penrose p( 0, Coordinate( 0.5, -0.5 ), 36, 0.05);
+
+        p.execute();      
+
+        float *vertices = p.GetVerticesWithColors();
+        int numVertices = p.GetNumTriangles() * 18;
+        /*for( int i = 0; i < numVertices; i += 6 ){
+            std::cout << "Coordinate:" << std::endl;
+            std::cout << "x: " << vertices[ i ] << " | y: " << vertices[ i + 1 ] << " | z: " << vertices[ i + 2 ] << std::endl;
+
+            std::cout << "RGB:" << std::endl;
+            std::cout << "R: " << vertices[ i + 3 ] << " | G: " << vertices[ i + 4 ] << " | B: " << vertices[ i + 5 ] << std::endl;
+        }*/
+
+        VertexArray va;
+
+        VertexBuffer vb( vertices, numVertices * sizeof( float ) );
+
+        VertexBufferLayout layout;
+        layout.Push<float>( 3 );
+        layout.Push<float>( 3 );
+        va.AddBuffer( vb, layout );
+        va.Bind();
+         
+        Shader shader( "res/shaders/animation_1.shader" );
+        shader.Bind();
+
+        va.UnBind();
+        shader.UnBind();
+        vb.UnBind();
+
+        Renderer renderer;
+
+        
+        glPolygonMode( GL_FRONT, GL_FILL );
+        shader.Bind();
+
+        float x = -0.03f;
+        float y = 0.06f;
+        float incremento = 0.001f;
+        /* Loop until the user closes the window */
+        while( !glfwWindowShouldClose( window ) ){
+            /* Render here */
+            renderer.Clear();
+
+            processInput( window );
+            
+            vb.Bind();
+            va.Bind();
+            GLCall( glDrawArrays( GL_TRIANGLES, 0, numVertices ) );
+            
+            glm::mat4 trans = glm::mat4( 1.0f );
+            trans = glm::scale( trans, glm::vec3( 1.0f, 1.0f, 0.0f ));
+
+            trans = glm::translate( trans, glm::vec3( x, 0.0f, 0.0f ) );
+
+            shader.SetuniformsMat4( "transform", trans );
+           
+
+            if( x >= 1.0f || x <= -1.0f ){
+                incremento *= -1;
+            }
+            if( y >= 1.0f || y <= -1.0f ){
+                incremento *= -1;
+            }
+            x += incremento;
+            y -= incremento;
+            //std::cout << "x: " << x << " | y: " << y << std::endl;
+
+            /* Swap front and back buffers */
+            GLCall( glfwSwapBuffers( window ) );
+
+            /* Poll for and process events */
+            GLCall( glfwPollEvents() );
+
+        }
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame andreact accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput( GLFWwindow * window ){
+    if( glfwGetKey( window, GLFW_KEY_ESCAPE ) == GLFW_PRESS )
+        glfwSetWindowShouldClose( window, true );
+    if( glfwGetKey( window, GLFW_KEY_W ) == GLFW_PRESS ){
+        GLCall( glPolygonMode( GL_FRONT_AND_BACK, GL_LINE ) );
+        std::cout << "w press" << std::endl;
+    }
+    if( glfwGetKey( window, GLFW_KEY_S ) == GLFW_PRESS ){
+        GLCall( glPolygonMode( GL_FRONT_AND_BACK, GL_FILL ) );
+        std::cout << "S press" << std::endl;
+    }
+    if( glfwGetKey( window, GLFW_KEY_A ) == GLFW_PRESS ){
+        GLCall( glPolygonMode( GL_FRONT_AND_BACK, GL_POINT ) );
+        std::cout << "a press" << std::endl;
+    }
+}
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback( GLFWwindow * window, int width, int height ){
+    // make sure the viewport matches the new window dimensions; note that width and
+    // height will be significantly larger than specified on retina displays.
+    glViewport( 0, 0, width, height );
+}

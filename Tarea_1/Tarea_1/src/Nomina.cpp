5c0e0fec47c5dbead617e83f370e1619f6de3bfe
Bruno Vitte <bruno.vitte@itam.mx>
2021-11-03 19:49:00 -0600

    Trabajando duro o durando en el trabajo?

--- Tarea_1/Tarea_1/src/Nomina.cpp
+++ Tarea_1/Tarea_1/src/Nomina.cpp
@@ -25,7 +25,23 @@ Empleado *Nomina::crear_empleado(){
 	char Estado_Civil;
 	int Numero_Hijos;
 
-	std
+	std::cout << "Ingresa Numero de Identificacion: " << std::endl;
+	std::cin >> Numero_Identificacion;
+	std::cout << "Ingresa Nombre: " << std::endl;
+	std::cin >> Nombre;
+	std::cout << "Ingresa Apellidos: " << std::endl;
+	std::cin >> Apellidos;
+
+	Direccion_Empleado = Create_Direccion();
+
+	std::cout << "Ingresa Salario: " << std::endl;
+	std::cin >> Salario;
+	std::cout << "Ingresa Genero (M) Masculino, (F) Femenino, (O) Otro: " << std::endl;
+	std::cin >> Genero;
+	std::cout << "Ingresa Estado Civil (C) Casado, (U) Unión Libre, (S) Soltero, (D) Divorciado, (V) Viudo, (O) Otro: " << std::endl;
+	std::cin >> Estado_Civil;
+	std::cout << "Ingresa Numero de Hijos: " << std::endl;
+	std::cin >> Numero_Hijos;
 
 	Empleado emp( Nombre, Apellidos, Numero_Identificacion, Direccion_Empleado, Salario, Genero, Estado_Civil, Numero_Hijos );
 	
@@ -33,6 +49,19 @@ Empleado *Nomina::crear_empleado(){
 }
 
 int Nomina::insertar_empelado(){
+	Empleado *Empleado_A_Insertar = crear_empleado();
+
+	if( ListaEmpleados == NULL ){
+
+		ListaEmpleados = Empleado_A_Insertar;
+		return 1;
+
+	} else {
+
+
+
+	}
+
 	return 0;
 }
 
